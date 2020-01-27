d21bfbd968b12970d24108e63e077f0897a57284
gbanc <gabe.banciu@gmail.com>
2020-01-26 22:50:07 -0800

    add verbose name Seller Accounts to profiles app, minor fixes

--- profiles/models.py
+++ profiles/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     seller_id = models.TextField(null=True)
     mws_auth_token = models.TextField(null=True)
     marketplace_id = models.TextField(null=True, default='ATVPDKIKX0DER', help_text='Defaults to US marketplace ID')
+
+    def __str__(self):
+        return '%s' % (self.seller_id)
